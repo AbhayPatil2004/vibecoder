ea6581571d949384db33a39512763e6150f043fa
Abhay Ramkrushna Patil <patilabhay484@gmail.com>
2025-11-16 00:07:42 +0530

    Prisma Client and MongoDb setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button>Get Started</Button>
